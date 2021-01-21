55e149acdc87e07a3770d92ca95c50a0ce62a3c5
DESKTOP-BGRH97B\Александр <sashakaliberda@gmail.com>
2021-01-21 13:45:14 +0300

    StockList

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alex-dev-master/codewars
+
+go 1.15
--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
-	"strings"
+	"strings",
 )
 
 func main() {
-	print(countSheep(1))
+	//countSheep
+	//print(countSheep(1))
+
+	//StockList
+	var b = []string{"BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"}
+	var c = []string{"A", "B", "C", "D"}
+	print(StockList(b,c))
 }
 
+
+func StockList(listArt []string, listCat []string) string {
+	for _, value := range listArt {
+		valueArr := strings.Split(value, "")
+		fmt.Printf("%v\n", valueArr)
+		//checkArr, indx := InArray(valueArr[0], listCat)
+		fmt.Printf("%t , %d \n", checkArr, indx)
+	}
+
+	return ""
+}
 
 func countSheep(num int) string {
 	// Your code here!
